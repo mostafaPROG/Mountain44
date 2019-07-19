02a5817050bb58a9dced281d905332aa42c4d1f7
mostafaPROG <pro192016>
2019-07-19 09:02:41 +0430

    gdgd

--- app/src/main/java/ir/nilgroup/mountain44/MainActivity.kt
+++ app/src/main/java/ir/nilgroup/mountain44/MainActivity.kt
@@ -1,14 +1,12 @@
 package ir.nilgroup.mountain44
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val x=2
     }
 }
